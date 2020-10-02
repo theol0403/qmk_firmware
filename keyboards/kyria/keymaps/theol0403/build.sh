70b217278a06a9d7b4c303bd3f32ad5c9f116d7f
Theo Lemay <theol0403@gmail.com>
2020-10-02 16:47:39 -0700

    Build tweak

--- keyboards/kyria/keymaps/theol0403/build.sh
+++ keyboards/kyria/keymaps/theol0403/build.sh
@@ -3,12 +3,12 @@ cd /home/theol/Documents/qmk
 
 if [[ $1 -eq 0 ]]
 then
-make kyria/rev1:theol0403:flash HAND=MASTER_RIGHT
-make kyria/rev1:theol0403:avrdude HAND=MASTER_LEFT
+make kyria/rev1:theol0403:flash HAND=RIGHT
+make kyria/rev1:theol0403:avrdude HAND=LEFT
 elif [[ $1 -eq 1 ]]
 then
-make kyria/rev1:theol0403:flash HAND=MASTER_RIGHT
+make kyria/rev1:theol0403:flash HAND=RIGHT
 elif [[ $1 -eq 2 ]]
 then
-make kyria/rev1:theol0403:avrdude HAND=MASTER_LEFT
+make kyria/rev1:theol0403:avrdude HAND=LEFT
 fi
--- keyboards/kyria/keymaps/theol0403/rules.mk
+++ keyboards/kyria/keymaps/theol0403/rules.mk
@@ -1,12 +1,20 @@
+ifndef HAND
+HAND = RIGHT
+endif
+
 OLED_DRIVER_ENABLE = yes   # Enables the use of OLED displays
 ENCODER_ENABLE = yes       # Enables the use of one or more encoders
 RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+
+ifeq ($(HAND),RIGHT)
+CFLAGS += -DMASTER_RIGHT
+
 MOUSEKEY_ENABLE = yes # Mouse keys
 EXTRAKEY_ENABLE = yes # Audio control and System control
 AUTO_SHIFT_ENABLE = yes # Auto Shift
 
-ifdef HAND
-CFLAGS += -D${HAND}
-else
-CFLAGS += -DMASTER_RIGHT
+else ifeq ($(HAND),LEFT)
+CFLAGS += -DMASTER_LEFT
+
 endif
+
