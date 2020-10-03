84d1038a9989fe6cbd23a5639336e5c4ab31a90b
Theo Lemay <theol0403@gmail.com>
2020-10-02 21:07:09 -0700

    clean

--- keyboards/kyria/keymaps/theol0403/keymap.c
+++ keyboards/kyria/keymaps/theol0403/keymap.c
@@ -1,6 +1,6 @@
 #include QMK_KEYBOARD_H
 
-#define __ KC_NO
+#define _ KC_NO
 
 enum layers { BASE, NUM, SYM, NAV, MOUS, FUNC, MDIA };
 
@@ -24,46 +24,46 @@ enum layers { BASE, NUM, SYM, NAV, MOUS, FUNC, MDIA };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT(
-  __, KC_K, KC_W, KC_L, KC_U, KC_QUES,                                      KC_V,    KC_D, KC_R, KC_QUOT, KC_Q, __,
-  __, HM_A, HM_T, HM_H, HM_E, KC_DOT,                                       KC_C,    HM_S, HM_N, HM_O,    HM_I, __,
-  __, KC_Z, KC_P, KC_F, KC_J, KC_COMM, __,      __,       __,      __,      KC_B,    KC_G, KC_W, KC_X,    KC_Y, __,
-                  __,   __,   THMB_L1, THMB_L2, THMB_L3,  THMB_R3, THMB_R2, THMB_R1, __,   __
+  _, KC_K, KC_W, KC_L, KC_U, KC_QUES,                                     KC_V,    KC_D, KC_R, KC_QUOT, KC_Q, _,
+  _, HM_A, HM_T, HM_H, HM_E, KC_DOT,                                      KC_C,    HM_S, HM_N, HM_O,    HM_I, _,
+  _, KC_Z, KC_P, KC_F, KC_J, KC_COMM, _,       _,       _,       _,       KC_B,    KC_G, KC_W, KC_X,    KC_Y, _,
+                 _,    _,    THMB_L1, THMB_L2, THMB_L3, THMB_R3, THMB_R2, THMB_R1, _,    _
 ),
 [NUM] = LAYOUT(
-  __, KC_LBRC, KC_7, KC_8, KC_9, KC_RBRC,                         __, __,      __,      __,      RESET,   __,
-  __, KC_SCLN, KC_4, KC_5, KC_6, KC_EQL,                          __, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, __,
-  __, KC_GRV,  KC_1, KC_2, KC_3, KC_BSLS, __,   __,       __, __, __, __,      __,      KC_ALGR, __,      __,
-                     __,   __,   KC_DOT,  KC_0, KC_MINS,  __, __, __, __,      __
+  _, KC_LBRC, KC_7, KC_8, KC_9, KC_RBRC,                      _, _,       _,       _,       RESET,   _,
+  _, KC_SCLN, KC_4, KC_5, KC_6, KC_EQL,                       _, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, _,
+  _, KC_GRV,  KC_1, KC_2, KC_3, KC_BSLS, _,    _,       _, _, _, _,       _,       KC_ALGR, _,       _,
+                    _,    _,    KC_DOT,  KC_0, KC_MINS, _, _, _, _,       _
 ),
 [SYM] = LAYOUT(
-  __, KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR,                            __, __,      __,      __,      RESET,   __,
-  __, KC_COLN, KC_DLR,  KC_PERC, KC_CIRC, KC_PLUS,                            __, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, __,
-  __, KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_PIPE, __,      __,       __, __, __, __,      __,      KC_ALGR, __,      __,
-                        __,      __,      KC_LPRN, KC_RPRN, KC_UNDS,  __, __, __, __,      __
+  _, KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR,                         _, _,       _,       _,       RESET,   _,
+  _, KC_COLN, KC_DLR,  KC_PERC, KC_CIRC, KC_PLUS,                         _, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, _,
+  _, KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_PIPE, _,       _,       _, _, _, _,       _,       KC_ALGR, _,       _,
+                       _,       _,       KC_LPRN, KC_RPRN, KC_UNDS, _, _, _, _,       _
 ),
 [NAV] = LAYOUT(
-  __,  RESET,   __,      __,      __,      __,                      KC_AGIN, KC_UNDO, KC_CUT,  KC_COPY, KC_PSTE, __,
-  __,  KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, __,                      KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_CAPS, __,
-  __,  __,      KC_ALGR, __,      __,      __, __, __,  __, __,     KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_INS,  __,
-                                  __,      __, __, __,  __, KC_ENT, KC_BSPC, KC_DEL,  __,      __
+  _,  RESET,   _,       _,       _,       _,                       KC_AGIN, KC_UNDO, KC_CUT,  KC_COPY, KC_PSTE, _,
+  _,  KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, _,                       KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_CAPS, _,
+  _,  _,       KC_ALGR, _,       _,       _, _, _,  _,     _,      KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_INS,  _,
+                        _,       _,       _, _, _, KC_ENT, KC_BSPC, KC_DEL, _,       _
 ),
 [MOUS] = LAYOUT(
-  __, RESET,   __,      __,      __,      __,                            __,      __,      __,      __,      __, __,
-  __, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, __,                            KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, __, __,
-  __, __,      KC_ALGR, __,      __,      __, __, __,  __,      __,      KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, __, __,
-                        __,      __,      __, __, __,  KC_BTN1, KC_BTN3, KC_BTN2, __,      __
+  _, RESET,   _,       _,       _,       _,                          _,       _,       _,       _,       _, _,
+  _, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, _,                          KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _, _,
+  _, _,       KC_ALGR, _,       _,       _, _, _,  _,      _,        KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, _, _,
+                       _,       _,       _, _, _,  KC_BTN1, KC_BTN3, KC_BTN2, _,       _
 ),
 [FUNC] = LAYOUT(
-  __, KC_F12, KC_F7, KC_F8, KC_F9, KC_PSCR,                          __, __,      __,      __,      RESET,   __,
-  __, KC_F11, KC_F4, KC_F5, KC_F6, KC_SLCK,                          __, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, __,
-  __, KC_F10, KC_F1, KC_F2, KC_F3, KC_PAUS, __,     __,      __, __, __, __,      __,      KC_ALGR, __,      __,
-                     __,    __,    KC_APP,  KC_SPC, KC_TAB,  __, __, __, __,      __
+  _, KC_F12, KC_F7, KC_F8, KC_F9, KC_PSCR,                       _, _,       _,       _,       RESET,   _,
+  _, KC_F11, KC_F4, KC_F5, KC_F6, KC_SLCK,                       _, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, _,
+  _, KC_F10, KC_F1, KC_F2, KC_F3, KC_PAUS, _,      _,      _, _, _, _,       _,       KC_ALGR, _,       _,
+                    _,     _,     KC_APP,  KC_SPC, KC_TAB, _, _, _, _,       _
 ),
 [MDIA] = LAYOUT(
-  __, RESET,   __,      __,      __,      __,                            RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, __,
-  __, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, __,                            KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, __,      __,
-  __, __,      KC_ALGR, __,      __,      __, __, __,  __,      __,      __,      __,      __,      __,      __,      __,
-                        __,      __,      __, __, __,  KC_MSTP, KC_MPLY, KC_MUTE, __,      __
+  _, RESET,   _,       _,       _,       _,                          RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, _,
+  _, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, _,                          KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, _,       _,
+  _, _,       KC_ALGR, _,       _,       _, _, _,  _,       _,       _,       _,       _,       _,       _,       _,
+                       _,       _,       _, _, _,  KC_MSTP, KC_MPLY, KC_MUTE, _,       _
 )
 };
 
