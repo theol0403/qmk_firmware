bdaf0bad8100cc5798c639b49984c61294f0da57
Theo Lemay <theol0403@gmail.com>
2020-10-02 21:21:12 -0700

    Clean

--- keyboards/kyria/keymaps/theol0403/keymap.c
+++ keyboards/kyria/keymaps/theol0403/keymap.c
@@ -15,13 +15,14 @@ enum layers { BASE, NUM, SYM, NAV, MOUS, FUNC, MDIA };
 #define HM_I LGUI_T(KC_I)
 
 #define THMB_L1 LT(MDIA, KC_ESC)
-#define THMB_L2 LT(NAV , KC_SPC)
+#define THMB_L2 LT(NAV, KC_SPC)
 #define THMB_L3 LT(MOUS, KC_TAB)
 
-#define THMB_R3 LT(SYM , KC_ENT)
-#define THMB_R2 LT(NUM , KC_BSPC)
+#define THMB_R3 LT(SYM, KC_ENT)
+#define THMB_R2 LT(NUM, KC_BSPC)
 #define THMB_R1 LT(FUNC, KC_DEL)
 
+// clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT(
   _, KC_K, KC_M, KC_L, KC_U, KC_QUES,                                     KC_V,    KC_D, KC_R, KC_QUOT, KC_Q, _,
@@ -66,25 +67,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                        _,       _,       _, _, _,  KC_MSTP, KC_MPLY, KC_MUTE, _,       _
 )
 };
+// clang-format on
 
 #ifdef OLED_DRIVER_ENABLE
-oled_rotation_t oled_init_user(oled_rotation_t rotation) {
-    return OLED_ROTATION_180;
-}
-
-static void render_qmk_logo(void) {
-  static const char PROGMEM qmk_logo[] = {
-    0x80,0x81,0x82,0x83,0x84,0x85,0x86,0x87,0x88,0x89,0x8a,0x8b,0x8c,0x8d,0x8e,0x8f,0x90,0x91,0x92,0x93,0x94,
-    0xa0,0xa1,0xa2,0xa3,0xa4,0xa5,0xa6,0xa7,0xa8,0xa9,0xaa,0xab,0xac,0xad,0xae,0xaf,0xb0,0xb1,0xb2,0xb3,0xb4,
-    0xc0,0xc1,0xc2,0xc3,0xc4,0xc5,0xc6,0xc7,0xc8,0xc9,0xca,0xcb,0xcc,0xcd,0xce,0xcf,0xd0,0xd1,0xd2,0xd3,0xd4,0};
-
-  oled_write_P(qmk_logo, false);
-}
+oled_rotation_t oled_init_user(oled_rotation_t rotation) { return OLED_ROTATION_180; }
 
 static void render_status(void) {
-    // QMK Logo and version information
-    render_qmk_logo();
-
     // Host Keyboard Layer Status
     oled_write_P(PSTR("Layer: "), false);
     switch (get_highest_layer(layer_state)) {
@@ -112,17 +100,11 @@ static void render_status(void) {
         default:
             oled_write_P(PSTR("Undefined\n"), false);
     }
-
-    // Host Keyboard LED Status
-    uint8_t led_usb_state = host_keyboard_leds();
-    oled_write_P(IS_LED_ON(led_usb_state, USB_LED_NUM_LOCK) ? PSTR("NUMLCK ") : PSTR("       "), false);
-    oled_write_P(IS_LED_ON(led_usb_state, USB_LED_CAPS_LOCK) ? PSTR("CAPLCK ") : PSTR("       "), false);
-    oled_write_P(IS_LED_ON(led_usb_state, USB_LED_SCROLL_LOCK) ? PSTR("SCRLCK ") : PSTR("       "), false);
 }
 
 void oled_task_user(void) {
     if (is_keyboard_master()) {
-        render_status();  // Renders the current keyboard state (layer, lock, caps, scroll, etc)
+        render_status();
     } else {
     }
 }
