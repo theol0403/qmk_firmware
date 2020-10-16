25048ae7beedbbbc380e5075d7a7450438c06720
Theo Lemay <theol0403@gmail.com>
2020-10-15 21:54:16 -0700

    nosetup
    work

--- keyboards/kyria/keymaps/theol0403/rules.mk
+++ keyboards/kyria/keymaps/theol0403/rules.mk
@@ -5,15 +5,12 @@ endif
 OLED_DRIVER_ENABLE = yes   # Enables the use of OLED displays
 ENCODER_ENABLE = yes       # Enables the use of one or more encoders
 RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
-
-ifeq ($(HAND),RIGHT)
-CFLAGS += -DMASTER_RIGHT
-
 MOUSEKEY_ENABLE = yes # Mouse keys
 # AUTO_SHIFT_ENABLE = yes # Auto Shift
 
+ifeq ($(HAND),RIGHT)
+CFLAGS += -DMASTER_RIGHT
 else ifeq ($(HAND),LEFT)
 CFLAGS += -DMASTER_LEFT
-
 endif
 
