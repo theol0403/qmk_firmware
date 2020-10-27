d8215b553ee49bcf76359fbf601fd73a0ea770f1
Theo Lemay <theol0403@gmail.com>
2020-10-27 10:04:27 -0700

    game

--- keyboards/kyria/keymaps/theol0403/config.h
+++ keyboards/kyria/keymaps/theol0403/config.h
@@ -30,7 +30,7 @@
 
 // Prevent normal rollover on alphas from accidentally triggering mods.
 #define IGNORE_MOD_TAP_INTERRUPT
-#define BILATERAL_COMBINATIONS
+#define BILATERAL_COMBINATIONS 400
 
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
 #define TAPPING_FORCE_HOLD
