d885b73bd28513c883b7aac96f0b507d6c2a604b
Theo Lemay <theol0403@gmail.com>
2020-12-21 12:58:30 -0800

    tapp

--- keyboards/kyria/keymaps/theol0403/combos/symbol_shortcuts.h
+++ keyboards/kyria/keymaps/theol0403/combos/symbol_shortcuts.h
@@ -2,15 +2,15 @@
 #include "keycode.h"
 #include "quantum_keycodes.h"
 BEGIN_SECTION(SYMBOLS)
-COMB(sym_hi, S(KC_I), HM_H, HM_I)
+TAPP(sym_hi, S(KC_I), HM_H, HM_I)
 SUBS(sym_kc, "KC_", HM_K, KC_C)
 
-COMB(sym_col, KC_COLN, HM_N, KC_DOT)
-COMB(sym_exl, KC_EXLM, HM_N, KC_QUES)
-COMB(sym_scol, KC_SCOLON, HM_N, KC_COMMA)
+TAPP(sym_col, KC_COLN, HM_N, KC_DOT)
+TAPP(sym_exl, KC_EXLM, HM_N, KC_QUES)
+TAPP(sym_scol, KC_SCOLON, HM_N, KC_COMMA)
 
-COMB(sym_quot, S(KC_QUOT), HM_H, KC_QUOT)
-COMB(sym_slash, KC_BSLASH, HM_H, HM_SL)
+TAPP(sym_quot, S(KC_QUOT), HM_H, KC_QUOT)
+TAPP(sym_slash, KC_BSLASH, HM_H, HM_SL)
 
 COMB(space, KC_SPC, OSL(NAV), THMB_R2)
 COMB(back, KC_BSPC, KC_V, THMB_R1)
