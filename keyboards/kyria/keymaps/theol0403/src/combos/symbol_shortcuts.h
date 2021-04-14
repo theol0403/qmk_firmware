451d044433d32ac0770888091b0579aa07a09bef
Theo Lemay <theol0403@gmail.com>
2021-04-14 01:58:16 -0700

    fix combos

--- keyboards/kyria/keymaps/theol0403/src/combos/symbol_shortcuts.h
+++ keyboards/kyria/keymaps/theol0403/src/combos/symbol_shortcuts.h
@@ -2,33 +2,33 @@
 // accelerators are shortcuts to speed up specific tap-hold sequences
 // for example, shift and i can be pressed together to output I without waiting for tapping term or worrying about order
 #define BEGIN_ACCELERATORS combo_i
-TAPP(i, S(KC_I), U_H, U_I)  // speed up I
+TAPP(i, S(U_I), U_H, U_I)  // speed up I
 
-TAPP(col, KC_COLN, U_N, KC_DOT)        // speed up :
-TAPP(exl, KC_EXLM, U_N, KC_QUES)       // speed up !
-TAPP8(scol, KC_SCOLON, U_N, KC_COMMA)  // speed up ,
-TAPP(quot, S(KC_QUOT), U_H, KC_QUOT)   // speed up "
-TAPP8(slash, KC_BSLASH, U_H, U_SL)     // speed up "\"
+TAPP(col, KC_COLN, U_N, U_DOT)       // speed up :
+TAPP(exl, KC_EXLM, U_N, U_QUES)      // speed up !
+TAPP8(scol, KC_SCOLON, U_N, U_COMM)  // speed up ,
+TAPP(quot, S(KC_QUOT), U_H, U_QUOT)  // speed up "
+TAPP8(slash, KC_BSLASH, U_H, U_SL)   // speed up "\"
 
 TAPP8(dash, KC_MINS, T_L1, T_R2)  // speed up -
 TAPP(under, KC_UNDS, T_L1, T_R1)  // speed up _
 #define END_ACCELERATORS combo_under
 
 #define BEGIN_ACTIONS combo_dott
-COMB(dott, SENT, KC_DOT, T_R2)   // sentence end .
-COMB(ques, QUES, KC_QUES, T_R2)  // sentence end ?
+COMB(dott, SENT, U_DOT, T_R2)   // sentence end .
+COMB(ques, QUES, U_QUES, T_R2)  // sentence end ?
 
-COMB(caps, SMRTCAPS, KC_R, KC_D)  // trigger smart caps
+COMB(caps, SMRTCAPS, U_R, U_D)  // trigger smart caps
 
-SUBS(semi, ";\n", KC_W, KC_G)  // end line of c code
-SUBS(arrow, "->", KC_F, KC_J)  // arrow
+SUBS(semi, ";\n", U_W, U_G)  // end line of c code
+SUBS(arrow, "->", U_F, U_J)  // arrow
 
-// COMB(tab, A(KC_TAB), U_T, T_L3)  // alt tab
+// COMB(tab, A(U_TAB), U_T, T_L3)  // alt tab
 
 #define END_ACTIONS combo_arrow
 
 // combos that require that they are held
 #define BEGIN_HOLDS combo_back
 // allow backspace to autorepeat if pressed with thumb key above it
-COMB(back, KC_BSPC, KC_V, T_R1)
+COMB(back, KC_BSPC, U_V, T_R1)
 #define END_HOLDS combo_back
